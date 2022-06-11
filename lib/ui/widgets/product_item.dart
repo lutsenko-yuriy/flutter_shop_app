1fc70250947341e41b2b9b3964564d9e05d48070
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2022-06-11 21:49:53 +0200

    Add UI to product item

---
+++ lib/ui/widgets/product_item.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import '../../domain/product.dart';
+
+class ProductItem extends StatefulWidget {
+  final Product _product;
+
+  const ProductItem(this._product, {Key key}) : super(key: key);
+
+  @override
+  State<ProductItem> createState() => _ProductItemState();
+}
+
+class _ProductItemState extends State<ProductItem> {
+  bool _isFavorite = false;
+  bool _isInCart = false;
+
+  void _toggleFavorite() {
+    setState(() {
+      _isFavorite = !_isFavorite;
+    });
+  }
+
+  void _toggleInCart() {
+    setState(() {
+      _isInCart = !_isInCart;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+      child: Image.network(
+        widget._product.imageUrl,
+        fit: BoxFit.cover,
+      ),
+      footer: GridTileBar(
+          backgroundColor: Colors.black38,
+          leading: IconButton(
+            icon: Icon(_isFavorite ? Icons.favorite : Icons.favorite_border),
+            onPressed: _toggleFavorite,
+          ),
+          title: Text(
+            widget._product.title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon: Icon(_isInCart
+                ? Icons.remove_shopping_cart
+                : Icons.add_shopping_cart),
+            onPressed: _toggleInCart,
+          )),
+    );
+  }
+}
